8758095c2a88ba7f28a51e07acde091e71edffc8
minhcore <dpnhatminh11@gmail.com>
2026-01-20 19:27:25 +0700

    add(matrix.c/h): add matrix_copy function

--- src/main.c
+++ src/main.c
@@ -7,27 +7,41 @@ int main(void)
     struct matrix m_matrix;
     struct matrix p_matrix;
     struct matrix res_matrix;
+    struct matrix tmp_matrix;
+
+    mat_data_t tmp_buff[9];
 
     // clang-format off
-    mat_data_t trans_data[16] = {
-        1, 0, 0, 10,
-        0, 1, 0, 50,
-        0, 0, 1, 0,
-        0, 0, 0, 1
+    mat_data_t trans_data[9] = {
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    };
+    // clang-format on
+    
+    // clang-format off
+    mat_data_t point_data[6] = {
+         10,    11,
+         12,    13,
+         14,    15 
     };
     // clang-format on
 
-    mat_data_t point_data[4] = { 0, 0, 0, 1 };
-
     mat_data_t res_mat[4];
 
-    matrix_init(&res_matrix, 4, 1, res_mat);
-    matrix_init(&m_matrix, 4, 4, trans_data);
-    matrix_init(&p_matrix, 4, 1, point_data);
+    matrix_init(&res_matrix, 3, 2, res_mat);
+    matrix_init(&m_matrix, 3, 3, trans_data);
+    matrix_init(&p_matrix, 3, 2, point_data);
+    matrix_init(&tmp_matrix, 3, 3, tmp_buff);
 
-    if (matrix_mul(&m_matrix, &p_matrix, &res_matrix) == MATRIX_IVALID) {
+    if (matrix_mul(&m_matrix, &p_matrix, &res_matrix) == MATRIX_INVALID) {
         printf("Error!\n");
     } else {
         matrix_print(&res_matrix);
     }
+    printf("Copy m_matrix:\n");
+    matrix_copy(&m_matrix, &tmp_matrix);
+    matrix_print(&tmp_matrix);
+    
+
 }
